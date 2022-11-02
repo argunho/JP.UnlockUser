e9975c8767f37fad77a3452f68bca1b7c57c8924
argunho <aslan_argun@hotmail.com>
2022-11-02 09:56:22 +0100

    session history

--- UnlockUser/ClientApp/src/components/pages/SessionHistory.js
+++ UnlockUser/ClientApp/src/components/pages/SessionHistory.js
@@ -12,32 +12,11 @@ export default function SessionHistory() {
     const historyList = SessionPasswordsList();
     const history = useHistory()
 
-
     // Go to the current session history log page
     const openSessionHistory = (name) => {
-            history.push(`/manage-user/${name}`);
+        history.push(`/manage-user/${name}`);
     }
 
-    // historyList.length > 0 &&
-    // <>
-    //     <Button variant='text' onClick={() => setDropdown(!dropdown)} color={dropdown ? "primary" : "inherit"}>
-    //         Historik &nbsp;&nbsp;{dropdown ? <ArrowDropUp /> : <ArrowDropDown />}
-    //     </Button>
-    //     <ul className={`selected-list history-list dropdown-div ${dropdown ? "dropdown-open" : ""}`}>
-    //         {SessionPasswordsList().map((x, index) => (
-    //             <Tooltip arrow
-    //                 key={index}
-    //                 title={x.users?.length > 0 ? x.users.map((y, ind) => (
-    //                     <pre key={ind}><b><font color='#000000'>{y.username} :</font></b> {y.password}</pre>
-    //                 )) : <pre><b><font color='#000000'>Lösenord : </font></b> {x.password}</pre>}
-    //                 classes={{ tooltip: "tooltip tooltip-blue tooltip-margin", arrow: "arrow-blue" }}>
-    //                 <li onClick={() => openSessionHistory(x.username, x.users)}>
-    //                     {x.username.replace("%", " ")}
-    //                 </li>
-    //             </Tooltip>
-    //         ))}
-    //     </ul>
-    // </>
     return (
         <div className='interior-div result-div'>
             <List className='search-result'>
@@ -55,23 +34,26 @@ export default function SessionHistory() {
 
             {historyList.length > 0 ?
                 /* If history list exists */
-                historyList.map((h, index) => (
-                    <ListItem key={index}>
-                        <ListItemText onClick={() => h.users?.length > 0 ? setDropdown(!dropdown) : openSessionHistory(h.username)}
-                            primary={h.username.replace("%", " ")}
-                            secondary={h.password} />
+                <List>
+                    {historyList.map((h, index) => (
+                        <ListItem key={index} style={{display: "block"}}>
+                            <ListItemText style={{ width: "100%" }} onClick={() => h.users?.length > 0 ? setDropdown(!dropdown) : openSessionHistory(h.username)}
+                                primary={h.username.replace("%", " ")}
+                                secondary={h.password ? ("Lösenord: " + h.password) : ("Elever: " + h.users.length)} />
 
-                        {/* If is class members */}
-                        {h.users?.length > 0 && <List className={`selected-list history-list dropdown-div ${dropdown ? "dropdown-open" : ""}`}>
-                            {h.users.map((x, ind) => (
-                                <ListItem key={index}>
-                                    <ListItemText onClick={() => openSessionHistory(x.username)}
-                                        primary={h.username.replace("%", " ")}
-                                        secondary={h.password} />
-                                </ListItem>))}
-                        </List>}
-                    </ListItem>
-                )) :
+                            {/* If is class members */}
+                            {h.users?.length > 0 && <List style={{ width: "97%", margin: "auto" }} className={`dropdown-div ${dropdown ? "dropdown-open" : ""}`}>
+                                {h.users.map((x, ind) => (
+                                    <ListItem key={index} className='search-result'>
+                                        <ListItemText onClick={() => openSessionHistory(x.username)}
+                                            primary={x.username.replace("%", " ")}
+                                            secondary={"Lösenord: " + x.password} />
+                                    </ListItem>))}
+                            </List>}
+                        </ListItem>
+                    ))}
+                </List>
+                :
                 /* If history list is empty */
                 <Alert className='alert' severity="warning">
                     <AlertTitle>Ingen tillgänlig session historik</AlertTitle>
