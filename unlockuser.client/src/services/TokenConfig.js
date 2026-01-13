75ec007507275f17849e54556c4f599965f1d16b
Aslan Khadizov <aslan_argun@hotmail.com>
2026-01-13 11:43:34 +0100

    packeges

--- unlockuser.client/src/services/TokenConfig.js
+++ unlockuser.client/src/services/TokenConfig.js
@@ -1,11 +1,24 @@
+// Installed
+import { jwtDecode } from 'jwt-decode';
+
 export function TokenConfig(isForm) {
+
   // Jwt token to connect server
   const _token = localStorage.getItem("token") || sessionStorage.getItem("token");
 
-  const headers = isForm ? {}  : {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*'
-    }
+  // Logout if token is expired
+  const isExpired = IsTokenExpired(_token);
+
+  if (isExpired) {
+    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
+    window.location.pathname = "/session/logout";
+  }
+
+  const headers = isForm ? {} : {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*'
+  }
 
   return ({
     headers: {
@@ -15,9 +28,12 @@ export function TokenConfig(isForm) {
   })
 }
 
-// if (fetch)
-//     return ({
-//       'Authorization': `Bearer ${_token}`,
-//       'Access-Control-Allow-Origin': '*',
-//       'Access-Control-Allow-Headers': '*'
-//     })
+export function IsTokenExpired(jwtToken) {
+  if (!jwtToken) return false;
+
+  const { exp } = jwtDecode(jwtToken);
+  if (!exp)
+    return false; // "JWT does not contain exp claim"
+
+  return Date.now() >= exp * 1000;
+}
