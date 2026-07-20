9483223b7fef628db4edf65cdcab105d777abb09
Aslan Khadizov <aslan_argun@hotmail.com>
2026-07-20 20:39:55 +0200

    update data controller

--- UnlockUser.Server/Controllers/DataController.cs
+++ UnlockUser.Server/Controllers/DataController.cs
@@ -3,14 +3,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Newtonsoft.Json;
 using System.Text;
+using System.Xml.Linq;
 
 namespace UnlockUser.Server.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
 [Authorize]
-public class DataController(IHelpService helpService, ICredentialsService credentials, ILocalFileService localFileService, 
-                                        IConfiguration config, IMemoryCache memoryCache, IRefreshLockService lockService, ILogger<DataController> logger) : ControllerBase
+public class DataController(IHelpService helpService, ICredentialsService credentials, ILocalFileService localFileService,
+                                        IConfiguration config, IMemoryCache memoryCache, IRefreshLockService lockService, DashboardService dashboardService, ILogger<DataController> logger) : ControllerBase
 {
     private readonly IHelpService _helpService = helpService;
     private readonly ICredentialsService _credentials = credentials;
@@ -18,6 +19,7 @@ public class DataController(IHelpService helpService, ICredentialsService creden
     private readonly IConfiguration _config = config;
     private readonly IMemoryCache _memoryCache = memoryCache;
     private readonly IRefreshLockService _lockService = lockService;
+    private readonly DashboardService _dashboardService = dashboardService;
 
     private readonly ILogger<DataController> _logger = logger;
 
@@ -46,7 +48,7 @@ public class DataController(IHelpService helpService, ICredentialsService creden
                 Id = s.Name,
                 Primary = s.Name,
                 Secondary = s.Place
-            }).ToList() ?? []; 
+            }).ToList() ?? [];
 
             collections.Add("schools", schools);
             _logger.LogInformation("Gruppdata har laddats ner. Group: Skolor. Tid: {time}.", DateTime.Now.ToString("G"));
@@ -72,7 +74,6 @@ public class DataController(IHelpService helpService, ICredentialsService creden
     [HttpGet("groups/by/name/{name}")]
     public async Task<IActionResult> GetGroupsByName(string name)
     {
-        var groupModels = new List<UserViewModel>();
         var username = _credentials.GetClaim("username");
 
         bool isLoading = _lockService.IsLocked(username!);
@@ -80,9 +81,43 @@ public class DataController(IHelpService helpService, ICredentialsService creden
         if (isLoading)
             await Task.WhenAny(_lockService.GetWaitTask(username!), Task.Delay(90000));
 
+        var group_members = GetCachedUsersGroup(name);
+        if (group_members.Count > 0)
+            return Ok(group_members);
+
+        if (_lockService.TryStart(username!, out var waitTask))
+        {
+            try
+            {
+                var claim_roles = _credentials.GetClaim("roles");
+                var roles = claim_roles?.Split(',', StringSplitOptions.RemoveEmptyEntries);
+                bool openAccess = roles.Contains("Moderator", StringComparer.OrdinalIgnoreCase);
+
+                await _dashboardService.StoreUsersByGroup(username!, openAccess, [name]);
+
+                group_members = GetCachedUsersGroup(name);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Failed to set up dashboard data. Error: {ex.Message}");
+            }
+            finally
+            {
+                _lockService.Finish(username!);
+            }
+        }
+
+        return Ok(group_members);
+    }
+    #endregion
+
+    #region Private methods
+    private List<UserViewModel> GetCachedUsersGroup(string name)
+    {
+        var group_members = new List<UserViewModel>();
         var id = HttpContext.Session.Id;
         if (_memoryCache.TryGetValue(
-            $"groups_{id}", 
+            $"groups_{id}",
             out Dictionary<string, List<UserViewModel>>? cachedGroups))
         {
             bool supportModel = string.Equals(name.ToString(), "Support", StringComparison.OrdinalIgnoreCase);
@@ -96,15 +131,15 @@ public class DataController(IHelpService helpService, ICredentialsService creden
                    .Cast<string>()!
                  ];
 
-                groupModels = [.. groups.SelectMany(g => cachedGroups!.TryGetValue(g.ToLower(), out var value) ? value : [])];
+                group_members = [.. groups.SelectMany(g => cachedGroups!.TryGetValue(g.ToLower(), out var value) ? value : [])];
             }
             else
             {
-                groupModels = cachedGroups!.TryGetValue(name.ToLower(), out var value) ? value : [];
+                group_members = cachedGroups!.TryGetValue(name.ToLower(), out var value) ? value : [];
             }
         }
 
-        return Ok(groupModels);
+        return group_members;
     }
     #endregion
 }
